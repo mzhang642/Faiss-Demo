7c212b58454aa5cb0b1ce5b8c7e42f2a009ee108
mzhang642 <mingyuanzhang5010@gmail.com>
2023-11-17 20:35:14 -0600

    worked d3!

--- frontend/src/app/components/d3-graph/d3-graph.component.ts
+++ frontend/src/app/components/d3-graph/d3-graph.component.ts
@@ -1,4 +1,3 @@
-// d3-graph.component.ts
 import { Component, Input, OnInit, OnDestroy, ElementRef, ViewChild } from '@angular/core';
 import { SearchService } from '../../services/search.service';
 import * as d3 from 'd3';
@@ -6,13 +5,14 @@ import { Subscription } from 'rxjs';
 
 interface Node extends d3.SimulationNodeDatum {
   id: string;
-  // Define other properties your nodes may have
+  group: number;
+  data: any; // Define other properties your nodes may have
 }
 
 interface Link extends d3.SimulationLinkDatum<Node> {
   source: Node;
   target: Node;
-  // Define properties your links may have, like 'source' and 'target' which are required
+  value: number; // Define properties your links may have, like 'source' and 'target' which are required
 }
 
 interface GraphData {
@@ -30,11 +30,11 @@ export class D3GraphComponent implements OnInit, OnDestroy {
   @ViewChild('graphContainer', { static: true }) private graphContainer!: ElementRef;
   private graphSubscription!: Subscription;
 
-  constructor(private searchService: SearchService) {}
+  constructor(private searchService: SearchService) { }
 
   ngOnInit() {
     this.graphSubscription = this.searchService.graphData$.subscribe(graphData => {
-      console.log('Received graphData:', graphData);  
+      console.log('Received graphData:', graphData);
       if (graphData) {
         this.renderGraph(graphData as GraphData);
       }
@@ -42,35 +42,52 @@ export class D3GraphComponent implements OnInit, OnDestroy {
   }
 
   private renderGraph(graphData: GraphData): void {
-    // Clear the existing graph
     d3.select(this.graphContainer.nativeElement).selectAll('*').remove();
 
-    // Set up the simulation and add forces
-    // Now ensure that the id accessor function for forceLink properly returns the identifier
+    const linkValueByNodeId = new Map<string, number>();
+    graphData.links.forEach(link => {
+      const targetId = typeof link.target === 'object' ? link.target.id : link.target;
+      linkValueByNodeId.set(targetId, link.value);
+    });
+
+    const colorScale = d3.scaleLinear<string>()
+      .domain(d3.extent(graphData.links, d => d.value) as [number, number])
+      .range(['lightblue', 'darkblue']);
+
+    const sizeScale = d3.scaleLinear<number>()
+      .domain(d3.extent(graphData.links, d => d.value) as [number, number])
+      .range([5, 15]);
+
+    const svg = d3.select(this.graphContainer.nativeElement).append('svg')
+      .attr('width', this.graphContainer.nativeElement.offsetWidth)
+      .attr('height', this.graphContainer.nativeElement.offsetHeight);
+
     let simulation = d3.forceSimulation(graphData.nodes)
       .force('link', d3.forceLink<Node, Link>(graphData.links).id(d => d.id))
       .force('charge', d3.forceManyBody())
-      .force('center', d3.forceCenter(this.graphContainer.nativeElement.offsetWidth / 2, this.graphContainer.nativeElement.offsetHeight / 2));
+      .force('center', d3.forceCenter(
+        parseFloat(svg.attr('width')) / 2, 
+        parseFloat(svg.attr('height')) / 2
+      ));
 
-    // Create the link lines
-    let link = d3.select(this.graphContainer.nativeElement).append('svg')
-      .attr('width', this.graphContainer.nativeElement.offsetWidth)
-      .attr('height', this.graphContainer.nativeElement.offsetHeight)
-      .selectAll('line')
+    let link = svg.selectAll('line')
       .data(graphData.links)
       .enter().append('line')
       .attr('stroke-width', 2)
       .style('stroke', 'grey');
 
-    // Create the node circles
-    let node = d3.select(this.graphContainer.nativeElement).append('svg')
-      .attr('width', this.graphContainer.nativeElement.offsetWidth)
-      .attr('height', this.graphContainer.nativeElement.offsetHeight)
-      .selectAll('circle')
+    let node = svg.selectAll('circle')
       .data(graphData.nodes)
       .enter().append('circle')
-      .attr('r', 5)
-      .style('fill', 'blue');
+      .attr('r', d => {
+        if (d.group === 1) {
+          return 20;  // Fixed size for central node (group 1)
+        } else {
+          const linkValue = linkValueByNodeId.get(d.id);
+          return linkValue ? sizeScale(linkValue) : 5;
+        }
+      })
+      .style('fill', d => d.group === 1 ? 'green' : colorScale(linkValueByNodeId.get(d.id) ?? 0));
 
     // Drag functionality for nodes
     const drag = (simulation: d3.Simulation<Node, undefined>) => {
@@ -79,7 +96,7 @@ export class D3GraphComponent implements OnInit, OnDestroy {
         event.subject.fx = event.x;
         event.subject.fy = event.y;
       }
-      
+
       function dragged(event: d3.D3DragEvent<SVGCircleElement, Node, Node>) {
         event.subject.fx = event.x;
         event.subject.fy = event.y;
@@ -92,27 +109,24 @@ export class D3GraphComponent implements OnInit, OnDestroy {
       }
 
       return d3.drag<SVGCircleElement, Node>()
-          .on('start', dragstarted)
-          .on('drag', dragged)
-          .on('end', dragended);
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended);
     };
 
-    node.call(drag(simulation) as any); // Cast as any if there are still type issues
+    node.call(drag(simulation) as any); 
 
-    // Update positions each tick of the simulation
     simulation.on('tick', () => {
       link
-          .attr('x1', (d) => (d as Link).source.x!)
-          .attr('y1', (d) => (d as Link).source.y!)
-          .attr('x2', (d) => (d as Link).target.x!)
-          .attr('y2', (d) => (d as Link).target.y!);
+        .attr('x1', (d) => (d as Link).source.x!)
+        .attr('y1', (d) => (d as Link).source.y!)
+        .attr('x2', (d) => (d as Link).target.x!)
+        .attr('y2', (d) => (d as Link).target.y!);
 
       node
-          .attr('cx', d => (d as Node).x!)
-          .attr('cy', d => (d as Node).y!);
+        .attr('cx', d => (d as Node).x!)
+        .attr('cy', d => (d as Node).y!);
     });
-
-    console.log('Rendering graph with data:', graphData); // Add for debugging
   }
 
   ngOnDestroy() {
